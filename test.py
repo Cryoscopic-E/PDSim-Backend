0e2ad4e3da34f51960463a8907abd86c125942eb
Emanuele De Pellegrin <emanueledepellegrin@live.it>
2022-03-22 22:58:49 +0000

    start refactoring to functions

--- test.py
+++ test.py
@@ -8,37 +8,55 @@ from unified_planning.model.action import Action, InstantaneousAction, DurativeA
 from unified_planning.model.types import Type
 from unified_planning.model.effect import Effect
 from unified_planning.model.fnode import FNode
+from unified_planning.model.object import Object
+
+
+def problem_objects(obj: Object):
+    # print(obj.name(), obj.type().name())
+    pass
 
 
 def main():
-
     pddl_read = PDDLReader()
-    prob : Problem = pddl_read.parse_problem('./pddl/domain.pddl','./pddl/instance-1.pddl')
-    #action : Action = prob.action('UNLOAD-TRUCK')
+    problem: Problem = pddl_read.parse_problem('./pddl/domain.pddl', './pddl/instance-1.pddl')
 
-    for action in prob.actions():
-      print(f'Action Name: {action.name}')
+    print(problem.kind().features())
 
-      # Action's parameters  [name-type]
-      for action_param in action.parameters():
+    # ==== PROBLEM NAME ====
+    # print(problem.name)
+
+    # ==== OBJECTS ====
+    for obj in problem.all_objects():
+        problem_objects(obj)
+
+    # ==== TYPES ====
+    
+
+    # ==== INITIAL FLUENTS ====
+    # print(problem.initial_values())
+    
+    # ==== ACTIONS ====
+    for action in problem.actions():
+        # print(f'Action Name: {action.name}')
+
+        # Action's parameters  [name-type]
+        for action_param in action.parameters():
             param_name: str = action_param.name()
             type_name = action_param.type().name()
-            print('\t',param_name, f'<{type_name}>')
+            # print('\t',param_name, f'<{type_name}>')
 
-      # Action's effects [negated, name, arity, arguments (maps to parameters)]
-      if isinstance(action, InstantaneousAction):
-        for effect in action.effects():
-          fluent_node :FNode = effect.fluent()
-          fluent : Fluent = fluent_node.fluent()
-          is_negated = not effect.value().is_true()
-          arguments = fluent_node.args()
-          print(f'Fluent Name: {fluent.name()}')
-          print(f'Negated:{is_negated}')
-          print(f'Arity:{fluent.arity()}')
-          print(f'Args:{arguments}')
-          print('\n')
-
-      
+        # Action's effects [negated, name, arity, arguments (maps to parameters)]
+        if isinstance(action, InstantaneousAction):
+            for effect in action.effects():
+                fluent_node: FNode = effect.fluent()
+                fluent: Fluent = fluent_node.fluent()
+                is_negated = not effect.value().is_true()
+                arguments = fluent_node.args()
+                # print(f'Fluent Name: {fluent.name()}')
+                # print(f'Negated:{is_negated}')
+                # print(f'Arity:{fluent.arity()}')
+                # print(f'Args:{arguments}')
+                # print('\n')
 
 
 if __name__ == '__main__':
