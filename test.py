49085e8c1a7463587d9841db62f928d414d983b0
AugustusInvicta <theblacknightburn@gmail.com>
2023-01-16 00:14:22 +0000

    Moved to venv and Added planners

--- test.py
+++ test.py
@@ -12,16 +12,18 @@ from unified_planning.model.fnode import FNode
 
 def main():
     #pdsim_reader: PDSimReader = PDSimReader('./pddl/blocks-domain.pddl',  './pddl/blocks-instance.pddl')
-    #pdsim_reader: PDSimReader = PDSimReader('./pddl/blocks-domain.pddl',  './pddl/blocks-instance-2BLOCKS.pddl')
+    pdsim_reader: PDSimReader = PDSimReader('./pddl/depot-domain.pddl',  './pddl/depot-problem.pddl')
     #pdsim_reader: PDSimReader = PDSimReader('./pddl/logistics-domain.pddl', './pddl/logistics-instance.pddl')
     #pdsim_reader: PDSimReader = PDSimReader('./pddl/elevator-domain-ut.pddl', './pddl/elevator-problem-ut.pddl')
     #pprint.pprint(pdsim_reader.pdsim_representation())
-    pdsim_reader: PDSimReader = PDSimReader('./pddl/domain.pddl',  './pddl/problem1.pddl')
+    #pdsim_reader: PDSimReader = PDSimReader('./pddl/domain.pddl',  './pddl/problem1.pddl')
     #save to json
     with open('pdsim_representation.json', 'w') as outfile:
         json.dump(pdsim_reader.pdsim_representation(), outfile)
+    
+
     if pdsim_reader.problem is not None:
-        pdsim_solver: PDSimSolver = PDSimSolver(pdsim_reader.problem)
+        pdsim_solver: PDSimSolver = PDSimSolver(pdsim_reader.problem, pdsim_reader.domain_path, pdsim_reader.problem_path, 'pyperplan')
         pprint.pprint(pdsim_solver.solve())
     else:
         print("Error: could not parse problem")
