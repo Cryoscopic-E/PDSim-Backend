ad03cd4cc49460e72c89ef057e7285229784b90a
Emanuele <emanueledepellegrin@live.it>
2023-09-22 18:40:17 +0100

    pdsim server class

--- pdsim_server.py
+++ pdsim_server.py
@@ -10,49 +10,57 @@ from unified_planning.engines import PlanGenerationResultStatus
 from unified_planning.shortcuts import OneshotPlanner, get_all_applicable_engines
 from unified_planning.grpc.proto_writer import ProtobufWriter
 
-HOST = 'localhost'
-PORT = 5556
 
-
-def info(host, port):
-    print("--------------------")
-    print("--- PDSim Server ---")
-    print("--------------------")
-    print(f"Listening on {host}:{port}")
-
-
-def check_planner_status():
-    while True:
-        global planner_finished
-        if planner_finished:
-            print("Planner has finished")
-        else:
-            print("Planner is still working...")
-        time.sleep(5)  # Sleep for 5 seconds before checking again
-
-
-class UnityServer:
-    def __init__(self):
+class PdSimUnityServer:
+    def __init__(self, host='localhost', port='5556', problem=None, plan=None):
+        self.host = host
+        self.port = port
         self.pddl_reader = PDDLReader()
         self.proto_writer = ProtobufWriter()
-        self.planner: OneshotPlanner = None
-        self.active_problem = None
+
+        self.planner = None
+        self.planner_running = False
+
+        self.plan = None
+        self.plan_result_computed = False
+
+        if problem is not None:
+            self.active_problem = problem
+        else:
+            self.active_problem = None
+        self.main_thread = threading.Thread(target=self.server_loop)
+
+    def info(self):
+        print("--------------------")
+        print("--- PDSim Server ---")
+        print("--------------------")
+        print(f"Listening on {self.host}:{self.port}")
 
     def clear_problem(self) -> None:
         self.active_problem = None
 
-    def parse_pddl(self, domain_path, problem_path) -> Problem:
-        self.active_problem = self.pddl_reader.parse_problem(domain_path, problem_path)
-        return self.active_problem
+    def parse_problem(self, domain_path, problem_path):
+        if self.active_problem is not None:
+            self.clear_problem()
+        try:
+            self.active_problem = self.pddl_reader.parse_problem(domain_path, problem_path)
+        except Exception as e:
+            print(e)
+            return {'status': 'ERROR', 'msg': str(e)}
+        return {'status': 'OK', 'msg': 'Problem parsed successfully'}
 
     def get_problem(self) -> Problem:
         return self.active_problem
 
-    def convert_to_protobuf(self) -> str:
+    def convert_to_protobuf(self):
         if self.active_problem is None:
             return ''
-        parse = self.proto_writer.convert(self.active_problem)
-        return parse.SerializeToString()
+        try:
+            parse = self.proto_writer.convert(self.active_problem)
+        except Exception as e:
+            print(e)
+            return {'status': 'ERROR', 'msg': 'Could not convert to protobuf'}
+        return {'status': 'OK', 'msg': 'Problem converted successfully', 'data': parse.SerializeToString()}
 
     def set_planner(self, planner_name):
         if self.active_problem is None:
@@ -64,104 +72,46 @@ class UnityServer:
             return []
         return get_all_applicable_engines(self.active_problem)
 
-    def server_thread(self):
+    def server_loop(self):
         context = zmq.Context()
         socket: zmq.Socket = context.socket(zmq.REP)
-        socket.bind('tcp://{}:{}'.format(HOST, PORT))
+        socket.bind('tcp://{}:{}'.format(self.host, self.port))
         signal.signal(signal.SIGINT, signal.SIG_DFL)
-        info(HOST, PORT)
+        self.info()
 
         while True:
             request = socket.recv_json()
             if request['request'] == 'pddl':
-                # handle parse domain and problem and planning
+                self.clear_problem()
+
+                domain_path = request['domain_path']
+                problem_path = request['problem_path']
+
+                parse_result = self.parse_problem(domain_path, problem_path)
+
                 pass
             elif request['request'] == 'protobuf':
+                self.clear_problem()
                 # check if a protobuf representation exists
                 pass
-            elif request['request'] == 'planner':
-                # check status of the planner
-                pass
+            elif request['request'] == 'planner_status':
+                if self.planner is None:
+                    socket.send_json({'status': 'ERROR', 'msg': 'No planner selected'})
+                elif self.planner_running:
+                    socket.send_json({'status': 'OK', 'msg': 'Planner is running'})
+                else:
+                    socket.send_json({'status': 'OK', 'msg': 'Planner is not running'})
             else:
                 print("Unknown request")
 
             time.sleep(0.1)
 
+    def planner_loop(self):
+        if self.planner is None:
+            return
+        self.planner_running = True
+        self.plan_result_computed = False
+        self.plan = self.planner.solve()
+        self.planner_running = False
+        self.plan_result_computed = True
 
-# Function to handle requests from Unity
-def handle_unity_requests():
-    context = zmq.Context()
-    socket: zmq.Socket = context.socket(zmq.REP)
-    socket.bind('tcp://{}:{}'.format(HOST, PORT))
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-    info(HOST, PORT)
-
-    while True:
-        request = socket.recv_json()
-        if request['request'] == 'pddl':
-            # handle parse domain and problem and planning
-            pass
-        elif request['request'] == 'protobuf':
-            # check if a protobuf representation exists
-            pass
-        elif request['request'] == 'planner':
-            # check status of the planner
-            pass
-        else:
-            print("Unknown request")
-
-        time.sleep(0.1)
-
-
-def pdsim_run_pddl(domain_path, problem_path):
-    pddl_reader = PDDLReader()
-
-    # Parse the domain and problem files
-    problem = pddl_reader.parse_problem(domain_path, problem_path)
-
-
-def pdsim_run_protobuf(data):
-    # Create a PDDL reader
-    pddl_reader = PDDLReader()
-
-    # Parse the domain and problem files
-    problem = pddl_reader.parse_problem('./pddl/rover-temporal.pddl', './pddl/rover-temporal-problem.pddl')
-
-    # Get the environment
-    env = get_environment(problem)
-
-    # Get all applicable engines
-    engines = get_all_applicable_engines()
-
-    # Create a planner
-    planner = OneshotPlanner(env, engines)
-
-    # Run the planner
-    result = planner.run()
-
-    # Check the result
-    if result.status == PlanGenerationResultStatus.SUCCESS:
-        print("Success!")
-        print(result.plan)
-    elif result.status == PlanGenerationResultStatus.FAILURE:
-        print("Failure!")
-    elif result.status == PlanGenerationResultStatus.TIMEOUT:
-        print("Timeout!")
-    elif result.status == PlanGenerationResultStatus.ERROR:
-        print("Error!")
-    else:
-        print("Unknown status!")
-
-
-if __name__ == "__main__":
-    # Create separate threads for checking planner status, handling Unity requests, and simulating planner work
-    planner_thread = threading.Thread(target=check_planner_status)
-    unity_thread = threading.Thread(target=handle_unity_requests)
-
-    # Start the threads
-    planner_thread.start()
-    unity_thread.start()
-
-    # Wait for the threads to finish (you can add a mechanism for graceful exit)
-    planner_thread.join()
-    unity_thread.join()
