0355e5609d1cc0481d2f14a9b378323e6a366870
Emanuele De Pellegrin <emanueledepellegrin@live.it>
2023-09-25 20:52:43 +0100

    Rework Done

---
+++ examples/blocks_pdsim.py
@@ -0,0 +1,82 @@
+from pdsim_unity import pdsim_upf
+from unified_planning.model.types import BOOL
+from unified_planning.shortcuts import *
+
+
+def blocks_upf_problem():
+    # Create a new type (without parent)
+    block = UserType("block")
+
+    # The UP calls "fluents" both predicates and functions
+    # Each fluent has a type (BOOL for all fluents in this case) and might have parameters
+    clear = Fluent("clear", BOOL, obj=block)  # in PDDL this would be (clear ?obj - block)
+    on_table = Fluent("on-table", BOOL, obj=block)
+    arm_empty = Fluent("arm-empty", BOOL)
+    holding = Fluent("holding", BOOL, obj=block)
+    on = Fluent("on", BOOL, above=block, below=block)
+
+    # Pickup action with one parameter a precondition formula and some effects
+    pickup = InstantaneousAction("pickup", obj=block)
+    pickup.add_precondition(clear(pickup.obj) & on_table(pickup.obj) & arm_empty)
+    pickup.add_effect(holding(pickup.obj), True)
+    pickup.add_effect(clear(pickup.obj), False)
+    pickup.add_effect(on_table(pickup.obj), False)
+    pickup.add_effect(arm_empty, False)
+
+    # More actions...
+    putdown = InstantaneousAction("putdown", obj=block)
+    putdown.add_precondition(holding(putdown.obj))
+    putdown.add_effect(holding(putdown.obj), False)
+    putdown.add_effect(clear(putdown.obj), True)
+    putdown.add_effect(on_table(putdown.obj), True)
+    putdown.add_effect(arm_empty, True)
+
+    stack = InstantaneousAction("stack", obj=block, underobj=block)
+    # More than one precondition can be set (implicit conjunction)
+    stack.add_precondition(clear(stack.underobj) & holding(stack.obj))
+    stack.add_precondition(Not(Equals(stack.obj, stack.underobj)))
+    stack.add_effect(arm_empty, True)
+    stack.add_effect(clear(stack.obj), True)
+    stack.add_effect(on(stack.obj, stack.underobj), True)
+    stack.add_effect(clear(stack.underobj), False)
+    stack.add_effect(holding(stack.obj), False)
+
+    unstack = InstantaneousAction("unstack", obj=block, underobj=block)
+    unstack.add_precondition(on(unstack.obj, unstack.underobj) & clear(unstack.obj) & arm_empty)
+    unstack.add_effect(holding(unstack.obj), True)
+    unstack.add_effect(clear(unstack.underobj), True)
+    unstack.add_effect(on(unstack.obj, unstack.underobj), False)
+    unstack.add_effect(clear(unstack.obj), False)
+    unstack.add_effect(arm_empty, False)
+
+    # So far we just created objects in memory, we have not yet declared a problem
+    # A `Problem` is a planning instance, but as a mutable object it can be partially specified
+    # Here we just add fluents and actions, so we represent the "domain"
+    problem = Problem("blocksworld")
+    for f in [clear, on_table, arm_empty, holding, on]:
+        # We can specify arbitrary default initial values (particularly useful for numeric fluents)
+        problem.add_fluent(f, default_initial_value=False)
+    problem.add_actions([pickup, putdown, stack, unstack])
+
+    # Each letter is a block
+    objects = [Object(x, block) for x in "DEMO"]
+    problem.add_objects(objects)
+
+    # Set the initial state (all fluents are false by default because of the domain specification)
+    problem.set_initial_value(arm_empty, True)
+    for o in objects:
+        problem.set_initial_value(on_table(o), True)
+        problem.set_initial_value(clear(o), True)
+
+    # Set the goal
+    base = objects[0]
+    for o in objects[1:]:
+        problem.add_goal(on(base, o))
+        base = o
+
+    return problem
+
+
+if __name__ == '__main__':
+    upf_problem = blocks_upf_problem()
+    pdsim_upf(upf_problem, "fast-downward")
---
+++ pdsim_unity.py
@@ -0,0 +1,137 @@
+import zmq
+import signal
+import time
+import argparse
+
+from unified_planning.io.pddl_reader import PDDLReader
+from unified_planning.model import Problem
+from unified_planning.engines import PlanGenerationResult
+from unified_planning.shortcuts import OneshotPlanner, get_all_applicable_engines
+from unified_planning.grpc.proto_writer import ProtobufWriter
+
+
+class PdSimUnityServer:
+    def __init__(self, problem_model: Problem, plan_result: PlanGenerationResult, host, port):
+        self.host = host
+        self.port = port
+        self.proto_writer = ProtobufWriter()
+        self.problem = problem_model
+        self.plan_result = plan_result
+
+    def info(self):
+        print("--------------------")
+        print("--- PDSim Server ---")
+        print("--------------------")
+        print(f"Listening on {self.host}:{self.port}")
+
+    def convert_to_protobuf(self, model):
+        try:
+            parse = self.proto_writer.convert(model)
+        except Exception as exception:
+            print(exception)
+            return None
+        return parse.SerializeToString()
+
+    def server_loop(self):
+        context = zmq.Context()
+        socket: zmq.Socket = context.socket(zmq.REP)
+        socket.bind('tcp://{}:{}'.format(self.host, self.port))
+        signal.signal(signal.SIGINT, signal.SIG_DFL)
+        self.info()
+
+        while True:
+            request = socket.recv_json()
+            if request['request'] == 'problem':
+                converted_problem = self.convert_to_protobuf(self.problem)
+                if converted_problem is not None:
+                    socket.send(converted_problem)
+            elif request['request'] == 'plan':
+                converted_plan = self.convert_to_protobuf(self.plan_result)
+                if converted_plan is not None:
+                    socket.send(converted_plan)
+            else:
+                print("Unknown request")
+            time.sleep(0.1)
+
+
+def usage():
+    # host and port are optional
+    print('''Usage: python 
+                pdsim_unity.py --domain <domain_file> 
+                                --problem <problem_file> 
+                                [--planner <planner_name>]
+                                [--host <host_address>] 
+                                [--port <port>]
+                                ''')
+
+
+def select_planner(planners):
+    print("Select planner:")
+    for i, p in enumerate(planners):
+        print(f"{i}. {p}")
+    print("Enter number:")
+    while True:
+        try:
+            planner_index = int(input())
+            if planner_index < 0 or planner_index >= len(planners):
+                raise ValueError
+            break
+        except ValueError:
+            print("Invalid input, try again")
+    return planners[planner_index]
+
+
+def solve_problem(problem, planner_name):
+    applicable_planners = get_all_applicable_engines(problem.kind)
+    if planner_name not in applicable_planners:
+        print(f"Planner {planner_name} not applicable to problem {problem.kind}")
+        planner_name = select_planner(applicable_planners)
+    planner = OneshotPlanner(name=planner_name)
+    return planner.solve(problem)
+
+
+def launch_server(problem, result, host, port):
+    server = PdSimUnityServer(problem, result, host, port)
+    server.server_loop()
+
+
+def pdsim_pddl(domain_path, problem_path, planner_name, host='127.0.0.1', port='5556'):
+    try:
+        problem_pddl = PDDLReader().parse_problem(domain_path, problem_path)
+    except Exception as exception:
+        print(exception)
+        exit(1)
+    try:
+        result = solve_problem(problem_pddl, planner_name)
+    except Exception as exception:
+        print(exception)
+        exit(1)
+    launch_server(problem_pddl, result, host, port)
+
+
+def pdsim_upf(problem_upf, planner_name, host='127.0.0.1', port='5556'):
+    try:
+        result = solve_problem(problem_upf, planner_name)
+    except Exception as exception:
+        print(exception)
+        exit(1)
+    launch_server(problem_upf, result, host, port)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PDSim server')
+    parser.add_argument('--domain', type=str, help='Domain file')
+    parser.add_argument('--problem', type=str, help='Problem file')
+    parser.add_argument('--planner', type=str, default='fast-downward', help='Planner name')
+    parser.add_argument('--host', type=str, default='127.0.0.1', help='Host address')
+    parser.add_argument('--port', type=str, default='5556', help='Port')
+
+    args = parser.parse_args()
+
+    # error checking args
+    if args.domain is None or args.problem is None:
+        usage()
+        exit(1)
+    else:
+        # run server
+        pdsim_pddl(args.domain, args.problem, args.planner, args.host, args.port)
